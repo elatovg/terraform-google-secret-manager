11b9a702467baa3d4a5608e197ec5b849dc73e83
Karim Elatov <elatov@google.com>
2022-02-20 23:50:33 -0500

    M examples/kms/main.tf  M examples/kms/outputs.tf  M examples/kms/variables.tf  M examples/multiple/main.tf  M examples/multiple/outputs.tf  M examples/multiple/variables.tf  M examples/pubsub/main.tf  M examples/pubsub/outputs.tf  M examples/pubsub/variables.tf  M examples/simple/main.tf  M examples/simple/outputs.tf  M examples/simple/variables.tf  M kitchen.yml  M main.tf  M outputs.tf  M test/fixtures/secret-manager/main.tf  M test/fixtures/secret-manager/outputs.tf  M test/fixtures/secret-manager/variables.tf  M test/integration/secret-manager/controls/secret-manager.rb  M test/integration/secret-manager/inspec.yml  M variables.tf

--- examples/kms/variables.tf
+++ examples/kms/variables.tf
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 variable "project_id" {
   type        = string
   description = "The project ID to manage the Pub/Sub resources"
@@ -6,26 +22,4 @@ variable "project_id" {
 variable "region" {
   type        = string
   description = "Region to create kms key and secret replicas in"
-}
-
-variable "secrets" {
-  type = list(object({
-    name                  = string
-    automatic_replication = bool
-    user_managed_replication = list(object({
-      location     = string
-      kms_key_name = string
-    }))
-    labels = map(string)
-    topics = list(object({
-      name = string
-    }))
-    rotation = object({
-      next_rotation_time = string
-      rotation_period    = string
-    })
-    secret_data = string
-  }))
-  description = "The list of the secrets"
-  default     = []
 }
